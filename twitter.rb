ba21a558eb23dfe306495495272e1a6e7d3462af
Clifton Regis <github email address>
2016-07-27 18:48:17 -0400

    Done.

--- twitter.rb
+++ twitter.rb
@@ -6,6 +6,7 @@ class TwitterApi
 
   def initialize
     keys = YAML.load_file('application.yml')
+    @name = "Sophie"
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = keys['CONSUMER_KEY']
       config.consumer_secret     = keys['CONSUMER_SECRET']
@@ -13,21 +14,30 @@ class TwitterApi
       config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
     end
   end
-
+  def get_friends
+    client.friends
+  end
 
   def most_recent_friend
     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    #binding.pry
+    client.friends.first
   end
 
   def find_user_for(username)
     #find the twitter gem method that returns the correct user, given a username
+    client.user(username)
   end
 
   def find_followers_for(username)
-    #find the twitter gem method that returns the follows of a given user
+    #find the twitter gem method that returns the follows of a given user  
+     client.followers(username).take(10)      
   end
 
   def homepage_timeline
+    
+    client.home_timeline
+
     #find the twitter gem method that retreives the tweets from a user's timeline.
   end
   
@@ -43,3 +53,14 @@ end
 # puts tweet_client.find_user_for("USERNAME HERE")
 # puts tweet_client.find_followers_for("USERNAME HERE")
 # puts tweet_client.homepage_timeline
+
+###########
+
+# my_twitter_api_instance = TwitterApi.new
+# # automatically, for us, the #initialize method will be run
+# my_twitter_api_instance.client 
+# my_twitter_api_instance.name
+
+# sophies_best_instance = TwitterApi.new
+
+#=> 
